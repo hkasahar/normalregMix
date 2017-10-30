74b3e9c7582a946494f802073d3a171e38a0e8ac
Katsumi Shimotsu <shimotsu@e.u-tokyo.ac.jp>
2017-10-30 10:05:58 +0900

    Add documentation of normalregMixtest.env. Remove imports of doParallel and related packages because they are no longer used.

--- R/normalregMix.R
+++ R/normalregMix.R
@@ -1,3 +1,12 @@
+#' @description Environment that determines whether \code{normalregMix}
+#' is in the test mode.
+#' @export
+#' @title normalregMixtest.env
+#' @name normalregMixtest.env
+#' @format An object of class environment with two items. The first element, 
+#' \code{normalregMix.test.on}, is logical and sets whether the test mode is on.
+#' The second element, \code{normalregMix.test.seed}, is an integer that is the
+#' seed for random number generation.
 normalregMixtest.env <- new.env(parent = emptyenv())
 normalregMixtest.env$normalregMix.test.on <- FALSE
 normalregMixtest.env$normalregMix.test.seed <- 8888577
@@ -5,5 +14,5 @@ normalregMixtest.env$normalregMix.test.seed <- 8888577
 #' @useDynLib normalregMix
 #' @importFrom Rcpp evalCpp
 #' @exportPattern "^[[:alpha:]]+"
-#' @import stats utils doParallel RcppArmadillo foreach minpack.lm iterators parallel ggplot2
+#' @import stats utils RcppArmadillo minpack.lm parallel
 NULL
