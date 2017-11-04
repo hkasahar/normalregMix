987ec0c7b5cc3251bb1755ed6b8cb04b8445abca
Katsumi Shimotsu <shimotsu@Katsumis-MacBook-Pro.local>
2017-11-04 13:55:41 +0900

    Delete the part corresponding to LRT.penalized=TRUE. Further, revise documentation.

--- R/regmix_tests.R
+++ R/regmix_tests.R
@@ -1,4 +1,4 @@
-#' @description Sequentially performs the MEM test given the data for y and x on 
+#' @description Sequentially performs the modified EM test given the data for y and x on 
 #' the null hypothesis H_0: m = m_0 where m_0 is in {1, 2, ..., maxm};
 #' using this function is equivalent to calling normalmixMEMtestSeq with regressors 
 #' specified by x as a parameter.
@@ -7,40 +7,39 @@
 #' @name regmixMEMtestSeq
 #' @param y n by 1 vector of data for y.
 #' @param x n by q matrix of data for x.
-#' @param z n by p matrix of regressor associated with gam.
+#' @param z n by p matrix of regressor associated with gamma.
 #' @param maxm maximum number of components set as null hypothesis in the mixture.
 #' @param ninits number of randomly drawn initial values.
 #' @param maxit maximum number of iterations.
-#' @param nbtsp number of bootstrap observations; by default, it is set to be 199
+#' @param nbtsp number of bootstrap replicates. Default is 199.
 #' @param parallel Determines what percentage of available cores are used, 
-#' represented by a double in [0,1]. 0.75 is default.
+#' represented by a double in [0,1]. Default is 1.
 #' @param cl cluster used for parallelization; if it is \code{NULL}, the system will automatically
 #' create a new one for computation accordingly.
 #' @param crit.bootstrap.from minimum m in null hypothesis to have critical values 
-#' calculated from bootstrap for the test statistics
-#' @param significance.level Significance level used for rejecting a null hypothesis.
+#' calculated from bootstrap for the test statistics.
+#' @param significance.level significance level used for rejecting a null hypothesis.
 #' @return A list of with the following items:
-#' \item{alpha}{maxm by maxm matrix, whose i-th column is a vector of alphas estimated given the null hypothesis m_0 = i}
-#' \item{mu}{maxm by maxm matrix, whose i-th column is a vector of mus estimated given the null hypothesis m_0 = i}
-#' \item{sigma}{maxm by maxm matrix, whose i-th column is a vector of sigmas estimated given the null hypothesis m_0 = i}
-#' \item{beta}{A list of length maxm, whose i-th element is a q times i matrix of betas estimated given the null hypothesis m_0 = i}
-#' \item{gam}{maxm by maxm matrix, whose i-th column is a vector of gams estimated given the null hypothesis m_0 = i}
-#' \item{emstat}{A maxm vector of values of modified EM statistics of the model at m_0 = 1, 2, ..., maxm}
-#' \item{pvals}{A maxm by 3 matrix whose i-th row indicates a vector of p-values at k = 1, 2, 3}
-#' \item{aic}{A maxm vector of Akaike Information Criterion of the fitted model at m_0 = 1, 2, ..., maxm}
-#' \item{bic}{A maxm vector of Bayesian Information Criterion of the fitted model at m_0 = 1, 2, ..., maxm}
-#' \item{loglik}{A maxm vector of log-likelihood values of the model at m_0 = 1, 2, ..., maxm}
-#' \item{penloglik}{A maxm vector of penalized log-likelihood values of the model at m_0 = 1, 2, ..., maxm}
-#' \item{pmle.result}{a list of output from regmixPMLE under the number of components selected by sequantial hypothesis testing}
+#' \item{alpha}{\code{maxm} by \code{maxm} matrix, whose i-th column is a vector of alphas estimated under the null hypothesis m_0 = i.}
+#' \item{mu}{\code{maxm} by \code{maxm} matrix, whose i-th column is a vector of mus estimated under the null hypothesis m_0 = i.}
+#' \item{sigma}{\code{maxm} by \code{maxm} matrix, whose i-th column is a vector of sigmas estimated under the null hypothesis m_0 = i.}
+#' \item{beta}{A list of length \code{maxm}, whose i-th element is a q times i matrix of betas estimated under the null hypothesis m_0 = i.}
+#' \item{gam}{\code{maxm} by \code{maxm} matrix, whose i-th column is a vector of gams estimated under the null hypothesis m_0 = i.}
+#' \item{emstat}{\code{maxm} by 3 matrix whose i-th row is the value of the modified EM statistic for testing the null hypothesis \eqn{m_0 = i} at k = 1, 2, 3.}
+#' \item{pvals}{\code{maxm} by 3 matrix whose i-th row indicates a vector of p-values at k = 1, 2, 3.}
+#' \item{aic}{vector of Akaike Information Criterion of the fitted model at m_0 = 1, 2, ..., maxm.}
+#' \item{bic}{vector of Bayesian Information Criterion of the fitted model at m_0 = 1, 2, ..., maxm.}
+#' \item{loglik}{vector of log-likelihood values of the model at m_0 = 1, 2, ..., maxm.}
+#' \item{penloglik}{vector of penalized log-likelihood values of the model at m_0 = 1, 2, ..., maxm.}
+#' \item{pmle.result}{list of output from regmixPMLE under the number of components selected by sequantial hypothesis testing.}
 #' @examples
 #' data(faithful)
 #' attach(faithful)
 #' \dontrun{regmixMEMtestSeq(y = eruptions, x = waiting, parallel = 1)}
 regmixMEMtestSeq <- function (y, x, z = NULL, maxm = 3, ninits = 10, maxit = 2000,
-                              nbtsp = 199, parallel = 0, cl = NULL,
-                              crit.bootstrap.from = 3,
-                              significance.level = 0.05,
-                              LRT.penalized = FALSE) {
+                              nbtsp = 199, parallel = 1, cl = NULL,
+                              crit.bootstrap.from = 2,
+                              significance.level = 0.05) {
   # Compute the modified EM test statistic for testing H_0 of m components
   # against H_1 of m+1 components for a univariate finite mixture of normals
   if (significance.level >= 1 || significance.level < 0)
@@ -114,12 +113,10 @@ regmixMEMtestSeq <- function (y, x, z = NULL, maxm = 3, ninits = 10, maxit = 200
 
       cat(sprintf("Testing the null hypothesis of %d components\n", m))
 
-      an    <- anFormula(parlist = parlist, m = m, n = n, LRT.penalized = LRT.penalized)
+      an    <- anFormula(parlist = parlist, m = m, n = n)
       par1  <- regmixMaxPhi(y = y, x = x, parlist = parlist, z = z, an = an,
                             ninits = ninits, maxit = maxit)
-      emstat.m <- 2*(par1$loglik-loglik0)
-      if (LRT.penalized) # use the penalized log-likelihood.
-        emstat.m  <- 2*(par1$penloglik-loglik0)
+      emstat.m <- 2*(par1$penloglik-loglik0)
       
       # use the estimate of b as one of the initial values
       binit <- par1$coefficient
@@ -127,7 +124,7 @@ regmixMEMtestSeq <- function (y, x, z = NULL, maxm = 3, ninits = 10, maxit = 200
       cat(c("modified EM-test statitic ", sprintf('%.3f',emstat.m)),"\n")
       if (m <= crit.bootstrap.from) {
         em.out <- regmixCrit(y = y, x = x, parlist = parlist, z = z,
-                             values = emstat.m)
+                             parallel =  parallel, values = emstat.m)
         cat(c("asymptotic p-value       ", sprintf('%.3f',em.out$pvals)),"\n \n")
       } else {
         em.out <- regmixCritBoot(y = y, x = x, parlist=parlist, z = z,
@@ -177,62 +174,53 @@ regmixMEMtestSeq <- function (y, x, z = NULL, maxm = 3, ninits = 10, maxit = 200
   a
 }  # end regmixMEMtestSeq
 
-#' @description Performs MEM test given the data for y and x on 
+#' @description Performs the modified EM test given the data for y and x on 
 #' the null hypothesis H_0: m = m_0. Using this function is equivalent to 
 #' calling normalmixMEMtest with regressors specified by x as a parameter.
 #' @export
 #' @title regmixMEMtest
 #' @name regmixMEMtest
-#' @param y n by 1 vector of data for y
-#' @param x n by q matrix of data for x
-#' @param m The number of components in the mixture defined by a null hypothesis, m_0
-#' @param z n by p matrix of regressor associated with gamma
-#' @param tauset A set of initial tau value candidates
-#' @param an a term used for penalty function
-#' @param ninits The number of randomly drawn initial values.
-#' @param crit.method Method used to compute the variance-covariance matrix,
-#' one of \code{"none"}, \code{"asy"}, and \code{"boot"}. The default option is
-#' '\code{"none"}. When \code{method = "asy"}, the p-values are computed
-#' based on an asymptotic method. When \code{method = "OPG"},
-#' the p-values are generated by bootstrapping.
-#' @param nbtsp The number of bootstrap observations; by default, set as 199.
-#' @param cl Cluster used for parallelization; if it is \code{NULL}, the system
+#' @param y n by 1 vector of data for y.
+#' @param x n by q matrix of data for x.
+#' @param m number of components in the mixture defined by a null hypothesis, m_0.
+#' @param z n by p matrix of regressor associated with gamma.
+#' @param tauset set of initial tau value candidates.
+#' @param an tuning parameter used in the penalty function.
+#' @param ninits number of randomly drawn initial values.
+#' @param crit.method method used to compute the critical values, one of \code{"none"},
+#' \code{"asy"}, and \code{"boot"}. The default option is \code{"asy"}. When \code{method = "asy"},
+#' the p-values are computed using the asymptotic critical values. When \code{method = "boot"},
+#' the p-values are computed by bootstrap.
+#' @param nbtsp The number of bootstrap replicates. Default is 199.
+#' @param cl cluster used for parallelization; if it is \code{NULL}, the system
 #' will automatically generate a new one for computation accordingly.
 #' @param parallel Determines what percentage of available cores are used, 
-#' represented by a double in [0,1]. 0.75 is default.
-#' @param LRT.penalized Determines whether penalized likelihood is used in calculation of LRT
-#' statistic for likelihood in an alternative hypothesis.
-#' @return A list of class \code{normalMix} with items:
-#' \item{coefficients}{A vector of parameter estimates. Ordered as \eqn{
-#' '\alpha_1,\ldots,\alpha_m,\mu_1,\ldots,\mu_m,\sigma_1,\ldots,\sigma_m,\gam}.}
-#' \item{parlist}{The parameter estimates as a list containing alpha, mu, and
-#' sigma (and gamma if z is included in the model).}
-#' \item{vcov}{The estimated variance-covariance matrix.}
-#' \item{loglik}{The maximized value of the log-likelihood.}
-#' \item{penloglik}{The maximized value of the penalized log-likelihood.}
-#' \item{aic}{Akaike Information Criterion of the fitted model.}
-#' \item{bic}{Bayesian Information Criterion of the fitted model.}
-#' \item{postprobs}{n by m matrix of posterior probabilities for observations}
-#' \item{components}{n by 1 vector of integers that indicates the indices of components
-#' each observation belongs to based on computed posterior probabilities}
+#' represented by a double in [0,1]. Default is 1.
+#' @return A list of class \code{normalregMix} with items:
+#' \item{emstat}{vector of the value of the modified EM test statistic at k=1, 2, 3.}
+#' \item{pvals}{vector of p-values at k = 1, 2, 3.}
+#' \item{crit}{When \code{crit.method = "asy"}, \code{crit} is vector of critical values at the 0.1, 0.05, 0.01 level.
+#'  When \code{crit.method = "boot"}, \code{crit} is 3 by 3 matrix of critival values at the (0.1, 0.05, 0.01) level, jth row corresponding to k=j.}
+#' \item{crit.method}{method used to compute the variance-covariance matrix.}
+#' \item{parlist}{parameter estimates as a list containing alpha, mubeta, and sigma (and gam if z is included in the model).}
 #' \item{call}{The matched call.}
-#' \item{m}{The number of components in the mixture.}
+#' \item{m}{number of components in the mixture defined by the null hypothesis, \eqn{m_0}.}
 #' @examples
 #' data(faithful)
 #' attach(faithful)
 #' \dontrun{regmixMEMtest(y = eruptions, x = waiting, m = 1, crit.method = "asy")}
-#' \dontrun{regmixMEMtest(y = eruptions, x = waiting, m = 2, parallel = 1, crit.method = "asy")}
+#' \dontrun{regmixMEMtest(y = eruptions, x = waiting, m = 2, crit.method = "asy")}
 regmixMEMtest <- function (y, x, m = 2, z = NULL, tauset = c(0.1,0.3,0.5),
                            an = NULL, ninits = 100,
                            crit.method = c("none", "asy", "boot"), nbtsp = 199,
                            cl = NULL,
-                           parallel = 0.75,
-                           LRT.penalized = FALSE) {
+                           parallel = 1) {
   # Compute the modified EM test statistic for testing H_0 of m components
   # against H_1 of m+1 components for a univariate finite mixture of normals
   y <- as.vector(y)
   x <- as.matrix(x)
   q <- ncol(x)
+  n <- length(y)
 
   if (!is.null(z))
     z <- as.matrix(z)
@@ -241,16 +229,13 @@ regmixMEMtest <- function (y, x, m = 2, z = NULL, tauset = c(0.1,0.3,0.5),
   loglik0 <- regmix.pmle.result$loglik
 
   if (is.null(an))
-    an <- anFormula(parlist = regmix.pmle.result$parlist, m = m, n = n, q = q, 
-                    LRT.penalized = LRT.penalized)
+    an <- anFormula(parlist = regmix.pmle.result$parlist, m = m, n = n, q = q)
 
   par1    <- regmixMaxPhi(y=y, x=x, parlist=regmix.pmle.result$parlist, z=z,
                           an=an, tauset = tauset, ninits=ninits)
   
   
-  emstat <- 2*(par1$loglik-loglik0)
-  if (LRT.penalized) # use the penalized log-likelihood.
-    emstat  <- 2*(par1$penloglik-loglik0)
+  emstat <- 2*(par1$penloglik-loglik0)
 
   if (crit.method == "asy"){
     result  <- regmixCrit(y=y, x=x, parlist=regmix.pmle.result$parlist, z=z, values=emstat,
