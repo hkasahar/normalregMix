f573c5ddbf8281aabeb2166ae7d4d35b8eea9bdd
Katsumi Shimotsu <shimotsu@e.u-tokyo.ac.jp>
2023-09-27 09:12:41 +0900

    correct cppRegmixPMLE() so that it returns the penalized loglikelihood at the updated parameter value. Remove from cppRegmixPMLE() the part concerning Z.

--- src/cppRegmixPMLE.cpp
+++ src/cppRegmixPMLE.cpp
@@ -9,15 +9,13 @@ const double SINGULAR_EPS = 10e-10; // criteria for matrix singularity
 //' Gaussian regressions by the EM algorithm.
 //' @export
 //' @title cppRegmixPMLE
-//' @name cppRegmixPMLE
-//' @param bs (m-1) + (q+2)m + p by ninits matrix of initial values of (alpha,mu,beta,sigma,gamma).
+//' @name  cppRegmixPMLE
+//' @param bs (3+q)*m by ninits matrix of initial values of (alpha,mu,beta,sigma).
 //' @param ys n by 1 vector of data for y.
 //' @param xs x n by q matrix of data for x.
-//' @param zs n by p matrix of regressor associated with gamma.
 //' @param mu0s m-1 vector of the estimate of mu from an m-1 component model.
 //' @param sigma0s m-1 vector of the estimate of sigma from an m-1 component model.
 //' @param m number of components in the mixture.
-//' @param p dimension of the regressor associated with gamma.
 //' @param an tuning parameter.
 //' @param cn tuning parameter.
 //' @param maxit maximum number of iterations.
@@ -34,28 +32,25 @@ const double SINGULAR_EPS = 10e-10; // criteria for matrix singularity
 //'
 // [[Rcpp::export]]
 List cppRegmixPMLE(NumericMatrix bs,
-                      NumericVector ys,
-                      NumericMatrix xs,
-                      NumericMatrix zs,
-                      NumericVector mu0s,
-                      NumericVector sigma0s,
-                      int m,
-                      int p,
-                      double an,
-                      double cn,
-                      int maxit = 2000,
-                      int ninits = 10,
-                      double tol = 1e-8,
-                      double tau = 0.5,
-                      int h = 0,
-                      int k = 0) {
+                   NumericVector ys,
+                   NumericMatrix xs,
+                   NumericVector mu0s,
+                   NumericVector sigma0s,
+                   int m,
+                   double an,
+                   double cn,
+                   int maxit = 2000,
+                   int ninits = 10,
+                   double tol = 1e-8,
+                   double tau = 0.5,
+                   int h = 0,
+                   int k = 0) {
   int n = ys.size();
   int q = xs.ncol();
   int q1 = q + 1;
   arma::mat b(bs.begin(), bs.nrow(), bs.ncol(), false);
   arma::vec y(ys.begin(), ys.size(), false);
   arma::mat x(xs.begin(), xs.nrow(), xs.ncol(), false);
-  arma::mat z(zs.begin(), zs.nrow(), zs.ncol(), false);
   arma::vec mu0(mu0s.begin(), mu0s.size(), false);
   arma::vec sigma0(sigma0s.begin(), sigma0s.size(), false);
   arma::vec b_jn(bs.nrow());
@@ -66,13 +61,8 @@ List cppRegmixPMLE(NumericMatrix bs,
   arma::mat w(m,n);
   arma::mat post(m*n,ninits);
   arma::vec notcg(ninits), penloglikset(ninits), loglikset(ninits);
-  arma::vec gamma(p);
-  arma::vec ytilde(n);
   arma::vec wtilde(n);
   arma::mat xtilde(n,q1);
-  arma::mat ztilde(n,p);
-  arma::mat zz(p,p);
-  arma::mat ze(p,1);
   int emit, sing;
   double oldpenloglik, s0j, diff, minr, w_j, sum_l_j, ssr_j, alphah, tauhat;
   arma::mat x1(n,q1);
@@ -104,18 +94,14 @@ List cppRegmixPMLE(NumericMatrix bs,
 
     /* initialize EM iteration */
     b_jn = b.col(jn);
+    alpha  = b_jn.subvec(0,m-1);
+    sigma  = b_jn.subvec((q1+1)*m,(q1+2)*m);
     for (int j=0; j < m; ++j){
-      alpha(j) = b_jn(j);
       for (int i=0; i<q1; ++i){
         mubeta(i,j) = b_jn(m+q1*j+i);
       }
-      sigma(j) = b_jn((q1+1)*m+j);
     }
-    if (p>0) {
-      for (int j=0; j < p; j++){
-        gamma(j) = b_jn((q1+2)*m+j);
-      }
-    }
+    
     oldpenloglik = R_NegInf;
     emit = 0;
     diff = 1.0;
@@ -126,15 +112,9 @@ List cppRegmixPMLE(NumericMatrix bs,
       ll = - (double)n * M_LN_SQRT_2PI; /* n/2 times log(2pi) */
       alp_sig = alpha/sigma;
 
-      if (p==0) {
-        ytilde = y;
-      } else {
-        ytilde = y - z*gamma;
-      }
-
       for (int i = 0; i < n; i++) {
         /* standardized squared residual */
-        r = (1.0/sigma) % ( ytilde(i) - trans(x1.row(i)*mubeta) );
+        r = (1.0/sigma) % ( y(i) - trans(x1.row(i)*mubeta) );
         r = 0.5 * (r % r); /* This is faster than r = pow( r, 2.0 ) */
         minr = min(r);
         /* posterior for i */
@@ -177,9 +157,9 @@ List cppRegmixPMLE(NumericMatrix bs,
           break;
         }
         
-        mubeta.col(j) = solve( design_matrix , trans(xtilde) * ytilde );
-        //mubeta.col(j) = solve( trans(xtilde) * x1 , trans(xtilde) * ytilde );
-        ssr_j = sum( trans(w.row(j)) % pow(  ytilde - x1*mubeta.col(j) , 2 ) );
+        mubeta.col(j) = solve( design_matrix , trans(xtilde) * y );
+        //mubeta.col(j) = solve( trans(xtilde) * x1 , trans(xtilde) * y );
+        ssr_j = sum( trans(w.row(j)) % pow(  y - x1*mubeta.col(j) , 2 ) );
         sigma(j) = sqrt( (ssr_j + 2.0*an*sigma0(j)*sigma0(j))  / (w_j + 2.0*an) );
         sigma(j) = fmax(sigma(j),0.05*sigma0(j));
         /* If k ==1, impose lower and upper bound */
@@ -195,7 +175,6 @@ List cppRegmixPMLE(NumericMatrix bs,
       /*   if k>1, we update tau              */
       if (k==1){
         alphah = (alpha(h-1)+alpha(h));
-/*        printf("%f", alphah); */
         alpha(h-1) = alphah*tau;
         alpha(h) = alphah*(1-tau);
       } else if (k>1) {
@@ -208,27 +187,6 @@ List cppRegmixPMLE(NumericMatrix bs,
         }
         alpha(h-1) = alphah*tau;
         alpha(h) = alphah*(1-tau);
-      }
-
-      if (p>0) { /* update gamma */
-        zz.zeros();
-        ze.zeros();
-        for (int j = 0; j < m; j++) {
-          wtilde = trans(w.row(j)) ;
-          for (int ii = 0; ii < p; ii++) {
-            ztilde.col(ii) = wtilde % z.col(ii);
-          }
-          zz = zz + ( trans(ztilde) * z ) / (sigma(j)*sigma(j));
-          ze = ze + ( trans(ztilde) * (y - x1*mubeta.col(j)) ) / (sigma(j)*sigma(j));
-        }
-        
-        if (rcond(zz) < SINGULAR_EPS)
-        {
-          sing = 1;
-          break;
-        }
-
-        gamma = solve(zz,ze);
       }
 
       /* Check singularity */
@@ -244,20 +202,45 @@ List cppRegmixPMLE(NumericMatrix bs,
         break;
       }
 
-    }/* EM loop ends */
+    } /* EM loop ends */
 
+    /* Compute loglik and penalized loglik under the updated parameter value */
+    alp_sig = alpha/sigma;
+      
+    ll = - (double)n * M_LN_SQRT_2PI; /* n/2 times log(2pi) */
+
+    for (int i = 0; i < n; i++) {
+      /* standardized squared residual */
+      r = (1.0/sigma) % ( y(i) - trans(x1.row(i)*mubeta) );
+      r = 0.5 * (r % r); /* This is faster than r = pow( r, 2.0 ) */
+      minr = min(r);
+      /* posterior for i */
+      /* normalizing with minr avoids the problem of dividing by zero */
+      l_j =  alp_sig % exp( minr-r );
+      sum_l_j = sum( l_j );
+      /* loglikelihood*/
+      ll +=  log(sum_l_j) - minr; /* subtract back minr */
+    } /* end for (i=0; i<n; i++) loop */
+
+    penloglik = ll + cn*log(2.0) + cn*fmin(log(tau),log(1-tau));
+    for (int j=0; j<m; j++) {
+      s0j = sigma0(j)/sigma(j);
+      penloglik += -an*(s0j*s0j - 2.0*log(s0j) -1.0);
+    }
+    
+    if (sing) {
+      ll = R_NegInf;
+      penloglik = R_NegInf;
+    }
+    
     penloglikset(jn) = penloglik;
     loglikset(jn) = ll;
+    b_jn.subvec(0,m-1) = alpha;
+    b_jn.subvec((q1+1)*m,(q1+2)*m) = sigma;
+    
     for (int j=0; j < m; j++){
-      b_jn(j) = alpha(j);
       for (int i=0; i<q1; ++i){
         b_jn(m+q1*j+i) = mubeta(i,j);
-      }
-      b_jn((q1+1)*m+j) = sigma(j);
-    }
-    if (p>0) {
-      for (int j=0; j < p; j++){
-        b_jn((q1+2)*m+j) = gamma(j);
       }
     }
     b.col(jn) = b_jn; /* b is updated */
@@ -271,4 +254,3 @@ List cppRegmixPMLE(NumericMatrix bs,
                             Named("post") = wrap(post)
   );
 }
-
